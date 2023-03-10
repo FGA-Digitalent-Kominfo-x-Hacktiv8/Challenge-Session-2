d4c5dfa8e3695dfe448cabc7d578ec93bab362c3
muhdjau <mhmd.jauhari@gmail.com>
2023-03-10 19:27:06 +0700

    Small-fix Commit

--- struktur_kontrol_for.go
+++ struktur_kontrol_for.go
@@ -9,15 +9,16 @@ func main() {
 		fmt.Println("Nilai i = ", i)
 	}
 
-	for j := 0; j <= 4; j++ {
+	for j := 0; j <= 10; j++ {
 		fmt.Println("Nilai j = ", j)
+		if j == 5 {
+			for idx, val := range "САШАРВО" {
+				fmt.Printf("Character %U '%c' starts at byte position %d \n", val, val, idx)
+			}
+		}
 	}
 
-	for idx, val := range "САШАРВО" {
-		fmt.Printf("Character %U '%c' starts at byte position %d \n", val, val, idx)
-	}
-
-	for j := 6; j <= 10; j++ {
-		fmt.Println("Nilai j = ", j)
-	}
+	// for j := 6; j <= 10; j++ {
+	// 	fmt.Println("Nilai j = ", j)
+	// }
 }
